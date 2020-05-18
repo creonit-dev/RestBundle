70b1b4eb9791ae1473f71d8b6c3daaa41d840cc2
Pavel Bondarovich <p.bondarovich@gmail.com>
2020-05-18 13:11:05 +0700

    SwaggerController configure

--- Controller/SwaggerController.php
+++ Controller/SwaggerController.php
@@ -5,18 +5,19 @@ namespace Creonit\RestBundle\Controller;
 use Creonit\RestBundle\Collector\SpecCollector;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class SwaggerController extends AbstractController
 {
     /**
      * @Route("/")
      */
-    public function index(SpecCollector $collector)
+    public function index(SpecCollector $collector, SerializerInterface $serializer)
     {
         $spec = $collector->collect();
 
         return $this->render('@CreonitRest/Swagger/index.html.twig', [
-            'spec' => $this->get('serializer')->normalize($spec),
+            'spec' => $serializer->normalize($spec),
         ]);
     }
 }
